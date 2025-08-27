91c30507ddcc30af19bd83fb130fe98acfa109d2
akiyoshitomita <akiyoshi_tomita@hotmail.com>
2025-08-27 21:29:12 +0900

    任意の場所へアニメーション移動を追加

--- docs/maps/maplibreMove.md
+++ docs/maps/maplibreMove.md
@@ -52,3 +52,39 @@ sidebar_position: 207
 import Map from '@site/src/components/map/maplibreMove';
 
 <Map></Map>
+
+## 指定した場所へ移動するアニメーションを表示する場合
+
+setCenter()の代わりにpanTo()を利用することにより、目的の場所までの移動がアニメーション表示に切り替わります。
+
+```typescript
+// 下の行を削除する
+// map.current.setCenter([lng, lat]).setZoom(zoom);
+
+// 下の行を追加する
+map.current.panTo([lng, lat]);
+```
+
+import Map2 from '@site/src/components/map/maplibreMove2';
+
+<Map2></Map2>
+
+panToを利用すると、同一Zoomレベルで目的の場所までアニメーションで移動を行います。
+Zoomの変更を同時に行うとアニメーションが止まってしまうので、zoomレベルも変更する場合は
+次の方法で行ってください。
+
+## アニメーションで移動とZoomを同時に変更する場合
+
+flyTo()を利用することで、Zoomと移動を組み合わせたアニメーションが行えます。
+元々、2点を飛行機のフライトのようにズームアウトし移動しズームインするような動きをします。
+
+
+```typescript
+map.current.flyTo({
+  center: [lng, lat],
+  zoom: zoom
+});
+```
+import Map3 from '@site/src/components/map/maplibreMove3';
+
+<Map3></Map3>
---
+++ src/components/map/maplibreMove2.js
@@ -0,0 +1,41 @@
+import React, { useRef, useEffect } from 'react';
+import maplibregl, {GlobeControl} from 'maplibre-gl';
+import 'maplibre-gl/dist/maplibre-gl.css';
+import './map.css';
+
+export default function Map2({ children, color }) {
+  const mapContainer = useRef(null);
+  const map = useRef(null);
+  const lng = 139.768;
+  const lat = 35.6844;
+  const zoom = 14;
+ 
+  function move2(){ 
+	  map.current.panTo([lng, lat]);
+  }
+
+  useEffect(() => {
+    if (map.current) return; // stops map from intializing more than once
+
+    map.current = new maplibregl.Map({
+      container: mapContainer.current,
+      style: `https://tile.openstreetmap.jp/styles/maptiler-basic-ja/style.json`,
+      center: [0, 0],
+      zoom: 0,
+    });
+   
+    new maplibregl.Marker({color: 'red'})
+                  .setLngLat([lng, lat])
+                  .addTo(map.current);
+
+  }, [lng, lat, zoom]);
+
+  return (
+    <div>
+      <div className="map-wrap">
+        <div ref={mapContainer} className="map" />
+      </div>
+      <button onClick={move2}>移動</button>
+    </div>
+  );
+}
